69df8fc9d520d9973e1ec990454ffe4b60bce644
Fernando Araujo <fernando@MacBook-Pro-de-Fernando.local>
2022-06-19 10:23:36 -0300

    feat(expression): repr and str dunders

--- solver/expression.py
+++ solver/expression.py
@@ -1,4 +1,17 @@
 
+_OPERADORES = {
+    "add": "+",
+    "sub": "-",
+    "mul": "*",
+    "truediv": "/",
+    "floordiv": "//",
+    "pow": "^",
+    "lt": "<",
+    "le": "<=",
+    "eq": "=",
+    "ge": ">=",
+    "gt": ">",
+}
 
 class Expression:
     def __init__(self, a, op, b) -> None:
@@ -9,4 +22,13 @@ class Expression:
     @property
     def value(self):
         return self.op(self.a.value, self.b.value)
+
+    def __repr__(self) -> str:
+        op_name = self.op.__name__
+        op = _OPERADORES.get(op_name, op_name)
+        return f"{self.a} {op} {self.b}"
             
+    def __str__(self) -> str:
+        op_name = self.op.__name__
+        op = _OPERADORES.get(op_name, op_name)
+        return f"({self.a} {op} {self.b})"
