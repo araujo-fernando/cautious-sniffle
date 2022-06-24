99a8c94fb8dc55e0c6215d76897856c243a298be
Fernando Araujo <flaraujojunior@gmail.com>
2022-06-24 14:05:12 -0300

    feat(variables): right and left dunder operators

--- solver/expression.py
+++ solver/expression.py
@@ -1,3 +1,4 @@
+import operator as op
 
 _OPERADORES = {
     "add": "+",
@@ -21,7 +22,16 @@ class Expression:
 
     @property
     def value(self):
-        return self.op(self.a.value, self.b.value)
+        try:
+            val_a = self.a.value
+        except AttributeError:
+            val_a = self.a
+        try:
+            val_b = self.b.value
+        except AttributeError:
+            val_b = self.b
+
+        return self.op(val_a, val_b)
 
     def __repr__(self) -> str:
         op_name = self.op.__name__
@@ -32,3 +42,69 @@ class Expression:
         op_name = self.op.__name__
         op = _OPERADORES.get(op_name, op_name)
         return f"({self.a} {op} {self.b})"
+
+    def __add__(self, other):
+        return Expression(self, op.add, other)
+
+    def __sub__(self, other):
+        return Expression(self, op.sub, other)
+
+    def __mul__(self, other):
+        return Expression(self, op.mul, other)
+
+    def __truediv__(self, other):
+        return Expression(self, op.truediv, other)
+
+    def __floordiv__(self, other):
+        return Expression(self, op.floordiv, other)
+
+    def __pow__(self, other):
+        return Expression(self, op.pow, other)
+
+    def __lt__(self, other):
+        return Expression(self, op.lt, other)
+
+    def __le__(self, other):
+        return Expression(self, op.le, other)
+
+    def __eq__(self, other):
+        return Expression(self, op.eq, other)
+
+    def __ge__(self, other):
+        return Expression(self, op.ge, other)
+
+    def __gt__(self, other):
+        return Expression(self, op.gt, other)
+
+    def __radd__(self, other):
+        return Expression(other, op.add, self)
+
+    def __rsub__(self, other):
+        return Expression(other, op.sub, self)
+
+    def __rmul__(self, other):
+        return Expression(other, op.mul, self)
+
+    def __rtruediv__(self, other):
+        return Expression(other, op.truediv, self)
+
+    def __rfloordiv__(self, other):
+        return Expression(other, op.floordiv, self)
+
+    def __rpow__(self, other):
+        return Expression(other, op.pow, self)
+
+    def __rlt__(self, other):
+        return Expression(other, op.lt, self)
+
+    def __rle__(self, other):
+        return Expression(other, op.le, self)
+
+    def __req__(self, other):
+        return Expression(other, op.eq, self)
+
+    def __rge__(self, other):
+        return Expression(other, op.ge, self)
+
+    def __rgt__(self, other):
+        return Expression(other, op.gt, self)
