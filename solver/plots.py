851d2c1ebeb6e46c7344d1b8dd6cf0c1f89bcfcb
Fernando Araujo <flaraujojunior@gmail.com>
2022-07-10 14:48:30 -0300

    feat(main): figures saving

--- solver/plots.py
+++ solver/plots.py
@@ -12,16 +12,25 @@ from pprint import pprint
 from tqdm import tqdm
 
 def normalize_evolution_data(evolution_data: list[list[list[float]]]):
+    objective_pen = [[sum(val) for val in it] for it in evolution_data]
     objectives = [[val[0] for val in it] for it in evolution_data]
     penalties = [[val[1] for val in it] for it in evolution_data]
 
+    for data in objective_pen:
+        data.sort()
     for data in objectives:
         data.sort()
     for data in penalties:
         data.sort()
 
+    objective_pen = list(map(list, zip(*objective_pen)))
     objectives = list(map(list, zip(*objectives)))
     penalties = list(map(list, zip(*penalties)))
+
+    objective_pen = np.array(objective_pen)
+    min_value = objective_pen.min()
+    max_value = objective_pen.max()
+    objective_pen = (objective_pen - min_value) / (max_value - min_value)
 
     objectives = np.array(objectives)
     min_value = objectives.min()
@@ -33,7 +42,7 @@ def normalize_evolution_data(evolution_data: list[list[list[float]]]):
     max_value = penalties.max()
     penalties = (penalties - min_value) / (max_value - min_value)
 
-    return objectives, penalties
+    return objective_pen, objectives, penalties
 
 
 def create_heatmaps(parameters: list, algo: str):
@@ -67,17 +76,24 @@ def create_heatmaps(parameters: list, algo: str):
         par = parameters[i]
         iter, pop, vars, constrs = par
         cenario = cenarios[(vars, constrs)]
-        
-        xticks = ['{0:d}'.format(i) if i % (iter//10) == 0 else '' for i in range(iter)]
-        yticks = ['{0:d}'.format(i) if i % (pop//2) == 0 else '' for i in range(pop)]
+
+        xlabels = ['{0:d}'.format(i) for i in range(1, iter+1, iter//10)]
+        xticks = [i for i in range(1, iter+1, iter//10)]
+        ylabels = ['{0:d}'.format(i) for i in range(1, pop+1, pop//10)]
+        yticks = [i for i in range(1, pop+1, pop//10)]
 
         evo_data = get_best_execution_data(par, algo)
-        objs, _ = normalize_evolution_data(evo_data)
+        pen_obj, objs, pens = normalize_evolution_data(evo_data)
         ax = plt.subplot(3, 2, i+1)
-        sns.heatmap(objs, annot=False, xticklabels=xticks, yticklabels=yticks, cbar_ax=cbar_ax, ax=ax)
-        ax.set_title(f"Cenário {cenario}")
+        sns.heatmap(objs, annot=False, cbar_ax=cbar_ax, ax=ax)
+        plt.xticks(xticks, xlabels)
+        plt.yticks(yticks, ylabels)
+        ax.set_title(f"Função Objetivo do Cenário {cenario}")
 
-    plt.show()
+    plt.setp(axn[-1, :], xlabel='Iteração')
+    plt.setp(axn[:, 0], ylabel='Indivíduo')
+    plt.savefig(f"figures/heat/{algo}_{pop}", dpi=200)
+    plt.close()
 
 def load_file_data(file_name: str):
     with open(file_name, "r") as f:
@@ -175,6 +191,67 @@ def create_resume_table(pso_params: list, de_params: list):
         f.write(comp_data)
         f.write(tail())
 
+
+def create_paretos(parameters: list, algo: str):
+    def get_best_execution_data(params: tuple, algo: str="de"):
+        iters, indiv, vars, constrs = params
+        experiment_files = [
+            path + f"{iters}it_{indiv}_ind{vars}var_{constrs}cnstr_{i}_{algo}.json"
+            for i in range(10)
+        ]
+
+        evo_data = [[[]]]
+        objectives = None
+        for experiment in experiment_files:
+            dados = load_file_data(experiment)
+            obj = dados["objectives"]
+            if objectives is None:
+                objectives = obj
+                evo_data = dados["evo_data"]
+            if sum(obj) > sum(objectives):
+                objectives = obj
+                evo_data = dados["evo_data"]
+
+        return evo_data
+
+    def pareto_frontier(data, maxX = True, maxY = True):
+        myList = sorted(data, reverse=maxX)
+        p_front = [myList[0]]    
+        for pair in myList[1:]:
+            if maxY: 
+                if pair[1] >= p_front[-1][1]:
+                    p_front.append(pair)
+            else:
+                if pair[1] <= p_front[-1][1]: 
+                    p_front.append(pair) 
+        p_frontX = [pair[0] for pair in p_front]
+        p_frontY = [pair[1] for pair in p_front]
+        return p_frontX, p_frontY
+
+    cenarios = {(87, 86): "A", (181, 153): "B", (251, 177): "C", (435, 268): "D", (559, 300): "E", (774, 420): "F"}
+
+    fig, axn = plt.subplots(3, 3, sharex=True, sharey=True, figsize=(10, 10))
+
+    for i in range(len(parameters)):
+        par = parameters[i]
+        iter, pop, vars, constrs = par
+        cenario = cenarios[(vars, constrs)]
+
+        evo_data = get_best_execution_data(par, algo)
+        data = [val for it in evo_data for val in it]
+        pareto_x, pareto_y = pareto_frontier(data)
+        # pen_obj, objs, pens = normalize_evolution_data(evo_data)
+        ax = plt.subplot(3, 3, i+1)
+        sns.scatterplot(x=np.array(pareto_x), y=np.array(pareto_y) )
+        ax.set_title(f"{pop} indivíduos")
+        plt.axes
+
+    plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))    
+    plt.setp(axn[-1, :], xlabel='Função Objetivo')
+    plt.setp(axn[:, 0], ylabel='Penalização')
+    plt.savefig(f"figures/pareto/{cenario}_{algo}", dpi=200)
+    plt.close()
+
 # ['solve_time', 'evo_data', 'num_vars', 'num_constrs', 'objectives',
 #  'solution_variables_values', 'population', 'max_iterations']
 
@@ -189,23 +266,35 @@ all_pso_params = list(set(re.match(regex, arq).groups() for arq in pso_files))
 all_de_params = list(set(re.match(regex, arq).groups() for arq in de_files))
 
 # iterations, population, variables, constraints
+# variables = {"87", "181", "251", "435", "559", "774"}
+# for vari in tqdm(variables):
+#     pso_params = [(int(p[0]), int(p[1]), int(p[2]), int(p[3])) for p in all_pso_params if p[0] == "1000" and p[2] == vari]
+#     de_params = [(int(p[0]), int(p[1]), int(p[2]), int(p[3])) for p in all_de_params if p[0] == "333" and p[2] == vari]
+
+#     pso_params = sorted(pso_params, key=lambda p: (p[1], p[2], p[3]))
+#     de_params = sorted(de_params, key=lambda p: (p[1], p[2], p[3]))
+
+#     create_resume_table(pso_params, de_params)
+
+# iterations, population, variables, constraints
 variables = {"87", "181", "251", "435", "559", "774"}
 for vari in tqdm(variables):
-    pso_params = [(int(p[0]), int(p[1]), int(p[2]), int(p[3])) for p in all_pso_params if p[0] == "1000" and p[2] == vari]
-    de_params = [(int(p[0]), int(p[1]), int(p[2]), int(p[3])) for p in all_de_params if p[0] == "333" and p[2] == vari]
+    pso_params = [(int(p[0]), int(p[1]), int(p[2]), int(p[3])) for p in all_pso_params if p[0] == "1000" and p[2] == vari and p[1] != "200"]
+    de_params = [(int(p[0]), int(p[1]), int(p[2]), int(p[3])) for p in all_de_params if p[0] == "333" and p[2] == vari and p[1] != "100"]
+
+    pso_params = sorted(pso_params, key=lambda p: (p[1], p[2], p[3]))
+    de_params = sorted(de_params, key=lambda p: (p[1], p[2], p[3]))
+    create_paretos(de_params, "de")
+    create_paretos(pso_params, "pso")
+
+# iterations, population, variables, constraints
+population = [500 - 50*val for val in range(10)]
+for pop in tqdm(population):
+    pso_params = [(int(p[0]), int(p[1]), int(p[2]), int(p[3])) for p in all_pso_params if p[0] == "1000" and int(p[1]) == pop]
+    de_params = [(int(p[0]), int(p[1]), int(p[2]), int(p[3])) for p in all_de_params if p[0] == "333" and int(p[1]) == pop//2]
 
     pso_params = sorted(pso_params, key=lambda p: (p[1], p[2], p[3]))
     de_params = sorted(de_params, key=lambda p: (p[1], p[2], p[3]))
 
-    create_resume_table(pso_params, de_params)
-
-# iterations, population, variables, constraints
-pso_params = [(int(p[0]), int(p[1]), int(p[2]), int(p[3])) for p in all_pso_params if p[0] == "1000" and p[1] == "50"]
-de_params = [(int(p[0]), int(p[1]), int(p[2]), int(p[3])) for p in all_de_params if p[0] == "333" and p[1] == "50"]
-
-pso_params = sorted(pso_params, key=lambda p: (p[1], p[2], p[3]))
-de_params = sorted(de_params, key=lambda p: (p[1], p[2], p[3]))
-
-pprint(pso_params)
-create_heatmaps(de_params, "de")
-create_heatmaps(pso_params, "pso")
+    create_heatmaps(de_params, "de")
+    create_heatmaps(pso_params, "pso")
